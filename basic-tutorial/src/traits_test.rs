7f45f5635d83e5dbd21c2ed1ee42c040ed2425f9
zhengshaodong <ngxcorpio@gmail.com>
2018-12-09 17:46:32 +0800

    Add traits tutorial

---
+++ basic-tutorial/src/traits_test.rs
@@ -0,0 +1,79 @@
+use std::fmt::Debug;
+use std::fmt::Display;
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {} ...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
+
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+
+pub fn notify(item: impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn notify_full<T: Summary>(item: T) {
+    notify(item);
+}
+
+#[allow(unused)]
+pub fn notify_two(item: impl Summary + Display) {}
+
+#[allow(unused)]
+pub fn notify_where<T, U>(t: T, u: U)
+    where T: Display + Clone,
+          U: Clone + Debug
+{}
+
+// 返回值中使用 `impl Trait` 语法
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_books"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
+    println!("1 new tweet: {}", returns_summarizable().summarize());
+}
