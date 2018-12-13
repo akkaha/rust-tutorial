c3669a36d55ecc3cbd580625aa4f6a0eb3aadf50
zhengshaodong <ngxcorpio@gmail.com>
2018-12-14 00:08:57 +0800

    Lifetimes tutorials

---
+++ basic-tutorial/src/lifetimes_test.rs
@@ -0,0 +1,19 @@
+#[allow(unused)]
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+// 生命周期注解并不改变任何引用的生命周期的长短。与当函数签名中指定了泛型类型参数后就可以接受任何类型一样，
+// 当指定了泛型生命周期后函数也能接受任何生命周期的引用。生命周期注解描述了多个引用生命周期相互的关系，而不影响其
+// 生命周期。
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
