f4077b44f57ebcfd2bf12e9a4a1e69570214a0c3
zhengshaodong <ngxcorpio@gmail.com>
2018-12-21 09:33:08 +0800

    Drop trait and std::memory:drop

---
+++ basic-tutorial/src/drop_test.rs
@@ -0,0 +1,17 @@
+#[allow(unused)]
+fn main() {
+    let c = CustomSmartPointer { data: String::from("my stuff") };
+    drop(c);
+    let d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointers created.")
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
