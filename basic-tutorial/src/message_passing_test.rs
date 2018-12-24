0152b886e2e9420cbfd418c01e811929fa91c12b
zhengshaodong <ngxcorpio@gmail.com>
2018-12-24 08:52:52 +0800

    Message passing tutorial

---
+++ basic-tutorial/src/message_passing_test.rs
@@ -0,0 +1,37 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("thread1:hi"),
+            String::from("thread1:from"),
+            String::from("thread1:the"),
+            String::from("thread1:thread"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("thread2:more"),
+            String::from("thread2:message"),
+            String::from("thread2:for"),
+            String::from("thread2:you"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("{}", received);
+    }
+}
